7c75208b0a9d7c424a67b2f9e24833408663c449
raner0824 <593074782@qq.com>
2015-03-29 21:36:23 +0800

    lalala

---
+++ web.sql
@@ -0,0 +1,57 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : fuck
+Source Server Version : 50617
+Source Host           : localhost:3306
+Source Database       : web
+
+Target Server Type    : MYSQL
+Target Server Version : 50617
+File Encoding         : 65001
+
+Date: 2015-03-29 21:35:29
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `fenye`
+-- ----------------------------
+DROP TABLE IF EXISTS `fenye`;
+CREATE TABLE `fenye` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  ` name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of fenye
+-- ----------------------------
+INSERT INTO `fenye` VALUES ('1', 'a');
+INSERT INTO `fenye` VALUES ('2', 'b');
+INSERT INTO `fenye` VALUES ('3', 'c');
+INSERT INTO `fenye` VALUES ('4', 'd');
+INSERT INTO `fenye` VALUES ('5', 'e');
+INSERT INTO `fenye` VALUES ('6', 'f');
+INSERT INTO `fenye` VALUES ('7', 'g');
+INSERT INTO `fenye` VALUES ('8', 'h');
+INSERT INTO `fenye` VALUES ('9', 'i');
+INSERT INTO `fenye` VALUES ('10', 'j');
+INSERT INTO `fenye` VALUES ('11', 'k');
+INSERT INTO `fenye` VALUES ('12', 'l');
+INSERT INTO `fenye` VALUES ('13', 'm');
+INSERT INTO `fenye` VALUES ('14', 'n');
+INSERT INTO `fenye` VALUES ('15', 'o');
+INSERT INTO `fenye` VALUES ('16', 'p');
+INSERT INTO `fenye` VALUES ('17', 'q');
+INSERT INTO `fenye` VALUES ('18', 'r');
+INSERT INTO `fenye` VALUES ('19', 's');
+INSERT INTO `fenye` VALUES ('20', 't');
+INSERT INTO `fenye` VALUES ('21', 'u');
+INSERT INTO `fenye` VALUES ('22', 'v');
+INSERT INTO `fenye` VALUES ('23', 'w');
+INSERT INTO `fenye` VALUES ('24', 'x');
+INSERT INTO `fenye` VALUES ('25', 'y');
+INSERT INTO `fenye` VALUES ('26', 'z');
+INSERT INTO `fenye` VALUES ('27', null);
